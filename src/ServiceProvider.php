ffda73889348cd7a85de5f06102adf69b87a012c
Ilia Lazarev <ilia.lazarev@icloud.com>
2020-04-12 10:04:20 +0300

    add merge config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -13,7 +13,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function register()
     {
-        //
+        $this->mergeConfigFrom(__DIR__ . '/../config/sqd.php', 'sqd');
     }
 
     /**
